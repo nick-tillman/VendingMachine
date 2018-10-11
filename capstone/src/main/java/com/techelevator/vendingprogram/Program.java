316b9a0515eca1523333f2da49f9eb69e75fd2ba
Nick Tillman <ndt6281@gmail.com>
2018-10-11 13:22:12 -0400

    made changes

--- capstone/src/main/java/com/techelevator/vendingprogram/Program.java
+++ capstone/src/main/java/com/techelevator/vendingprogram/Program.java
@@ -1,5 +1,30 @@
 package com.techelevator.vendingprogram;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
+
+	public static void main(String[] args) {
+		Scanner inputReader = new Scanner(System.in);
+
+		String path = "/users/ntillman/repos/other/team6-java-week4-pair-exercises/capstone/vendingmachine.csv";
+		File newFile = new File(path);
+		VendingMachine vm = new VendingMachine(newFile);
+		boolean done = false;
+		while (!done) {
+			log("What would you like to purchase?");
+			String userSelection = inputReader.nextLine();
+			vm.purchaseItem(userSelection);
+			vm.getList();
+		}
+
+	}
+
+	public static void log(String message) {
+		System.out.println(message);
+	}
+
+	// users/ntillman/repos/other/team6-java-week4-pair-exercises/capstone/vendingmachine.csv
 
 }
--- capstone/src/main/java/com/techelevator/vendingprogram/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/vendingprogram/VendingMachine.java
@@ -2,21 +2,25 @@ package com.techelevator.vendingprogram;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 
 public class VendingMachine {
-	
+
 	private static File setupFile = null;
 	private static Map<Item, Integer> inventory = new HashMap<Item, Integer>();
+	private Queue<Item> customerPurchases = new LinkedList<Item>();
 	private double transactionBalance;
 	private double totalSum;
-	
+
 	public VendingMachine(File setupFile) {
 		this.setupFile = setupFile;
 		setupMachine();
 	}
-	
+
 	public static void setupMachine() {
 		try (Scanner fileScanner = new Scanner(setupFile)) {
 			while (fileScanner.hasNextLine()) {
@@ -29,6 +33,23 @@ public class VendingMachine {
 			System.out.println("An exception occurred!");
 		}
 	}
-	
 
+	public void getList() {
+		Set<Item> keySet = inventory.keySet();
+		for (Item item : keySet) {
+			System.out.println(inventory.get(item));
+		}
+	}
+
+	public void purchaseItem(String userSelection) {
+		Set<Item> keySet = inventory.keySet();
+		for (Item item : keySet) {
+			if (item.getSlot().equals(userSelection)) {
+				int currentStock = inventory.get(item);
+				inventory.put(item, currentStock - 1);
+				customerPurchases.add(item);
+				
+			}
+		}
+	}
 }
