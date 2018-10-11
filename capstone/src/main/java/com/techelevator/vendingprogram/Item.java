a851de25ed6412ff40805a1c0f04d6b62350d69e
Matthew Schroeder <matthewschroederguitar@gmail.com>
2018-10-11 12:12:44 -0400

    Item class

--- capstone/src/main/java/com/techelevator/vendingprogram/Item.java
+++ capstone/src/main/java/com/techelevator/vendingprogram/Item.java
@@ -2,4 +2,42 @@ package com.techelevator.vendingprogram;
 
 public class Item {
 
+	private String slot;
+	private String name;
+	private double price;
+	private String type;
+
+	public String getSlot() {
+		return slot;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getSound() {
+		if (type == "chip") {
+			return "Crunch, Crunch, Yum!";
+		}
+		if (type == "candy") {
+			return "Munch Munch, Yum!";
+		}
+		if (type == "drink") {
+			return "Glug Glug, Yum!";
+		}
+		if (type == "gum") {
+			return "Chew Chew, Yum!";
+		}
+		return "";
+	}
+
 }
+	
